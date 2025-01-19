71e356bd98de28f15f38153070a5dbe514ee6e71
Brian Wyka <brian.wyka@logicmonitor.com>
2025-01-19 21:00:07 +0800

    bundle-jemalloc:  Bundle jemalloc with JDBC java library, Fixes #63

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,17 +1,17 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3741"
+#define DUCKDB_PATCH_VERSION "0"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
-#define DUCKDB_MINOR_VERSION 1
+#define DUCKDB_MINOR_VERSION 0
 #endif
 #ifndef DUCKDB_MAJOR_VERSION
-#define DUCKDB_MAJOR_VERSION 1
+#define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3741"
+#define DUCKDB_VERSION "v0.0.0"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ab8c909857"
+#define DUCKDB_SOURCE_ID "deadbeeff"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
