b04606f5e643b40b43f55ba297527324099e2730
Alex Kasko <alex@staticlibs.net>
2025-03-20 21:38:53 +0000

    Move jemalloc selection logic to CMake
    This change is a follow-up to #162, it moves platform-specific jemalloc
    selection logic from `vendor.py` to CMake. Additionally CMake file is
    restructured and cleaned up.
    
    Testing: no functional changes, no new tests.

--- vendor.py
+++ vendor.py
@@ -10,22 +10,11 @@ parser = argparse.ArgumentParser(description='Inlines DuckDB Sources')
 parser.add_argument('--duckdb', action='store',
                     help='Path to the DuckDB Version to be vendored in', required=True, type=str)
 
-
-
 args = parser.parse_args()
 
-
-# list of extensions to bundle
-extensions = ['core_functions', 'parquet', 'icu', 'json']
-
-# Conditionally include jemalloc
-is_android = hasattr(sys, 'getandroidapilevel')
-is_pyodide = 'PYODIDE' in os.environ
-use_jemalloc = (
-    not is_android and not is_pyodide and platform.system() == 'Linux' and platform.architecture()[0] == '64bit'
-)
-if use_jemalloc:
-  extensions.append('jemalloc')
+# list of extensions to bundle, we include jemalloc here to have its
+# files copied along with all other sources
+extensions = ['core_functions', 'parquet', 'icu', 'json', 'jemalloc']
 
 # path to target
 basedir = os.getcwd()
@@ -38,55 +27,47 @@ scripts_dir = 'scripts'
 sys.path.append(scripts_dir)
 import package_build
 
-defines = ['DUCKDB_EXTENSION_{}_LINKED'.format(ext.upper()) for ext in extensions]
-
-# # Autoloading is on by default for node distributions
-# defines.extend(['DUCKDB_EXTENSION_AUTOLOAD_DEFAULT=1', 'DUCKDB_EXTENSION_AUTOINSTALL_DEFAULT=1'])
-
-
-
-
-
+defines = ['DUCKDB_EXTENSION_{}_LINKED'.format(ext.upper()) for ext in extensions if ext != 'jemalloc']
 
 # fresh build - copy over all of the files
 (source_list, include_list, original_sources) = package_build.build_package(target_dir, extensions, False)
 
+# process jemalloc separately with its own CMake vars
+jemalloc_include_files = []
+jemalloc_include_list = []
+jemalloc_source_list = []
+jemalloc_dir = os.path.join(args.duckdb, 'extension', 'jemalloc')
+package_build.include_package('jemalloc', jemalloc_dir, jemalloc_include_files, jemalloc_include_list, jemalloc_source_list)
+jemalloc_source_list = [os.path.join('duckdb', x) for x in jemalloc_source_list]
 
-
-# # # the list of all source files (.cpp files) that have been copied into the `duckdb_source_copy` directory
-# # print(source_list)
-# # # the list of all include files
-# # print(include_list)
-source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x) for x in source_list]
-include_list = [os.path.join('src', 'duckdb', x) for x in include_list]
-
-libraries = []
+source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x)
+    for x in source_list if x not in jemalloc_source_list]
+jemalloc_source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x)
+    for x in jemalloc_source_list]
+include_list = [os.path.join('src', 'duckdb', x) for x in include_list if x not in jemalloc_include_list]
+jemalloc_include_list = [os.path.join('src', 'duckdb', x) for x in jemalloc_include_list]
 
 def sanitize_path(x):
     return x.replace('\\', '/')
 
-
 source_list = [sanitize_path(x) for x in source_list]
+jemalloc_source_list = [sanitize_path(x) for x in jemalloc_source_list]
 include_list = [sanitize_path(x) for x in include_list]
-libraries = [sanitize_path(x) for x in libraries]
+jemalloc_include_list = [sanitize_path(x) for x in jemalloc_include_list]
 
 os.chdir(basedir)
 
 with open('CMakeLists.txt.in', 'r') as f:
     cmake = f.read()
 
-
 def replace_entries(cmake, replacement_map):
     cmake.replace()
 
-
-cmake = cmake.replace('${SOURCE_FILES}', ' '.join(source_list))
-cmake = cmake.replace('${INCLUDE_FILES}', ' '.join(include_list))
-cmake = cmake.replace('${DEFINES}', ' '.join(['-D'+x for x in defines]))
-cmake = cmake.replace('${LIBRARY_FILES}', ' '.join(libraries))
-# cmake.replace('${CFLAGS}', cflags)
-# cmake.replace('${WINDOWS_OPTIONS}', windows_options)
-
+cmake = cmake.replace('${SOURCES}', '\n  '.join(source_list))
+cmake = cmake.replace('${JEMALLOC_SOURCES}', '\n  '.join(jemalloc_source_list))
+cmake = cmake.replace('${INCLUDES}', '\n  '.join(include_list))
+cmake = cmake.replace('${JEMALLOC_INCLUDES}', '\n  '.join(jemalloc_include_list))
+cmake = cmake.replace('${DEFINES}', '\n  '.join(['-D'+x for x in defines]))
 
 with open('CMakeLists.txt', 'w+') as f:
     f.write(cmake)
