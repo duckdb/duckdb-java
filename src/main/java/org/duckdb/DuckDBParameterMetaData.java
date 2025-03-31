7596fd522acf9f5fd38a1b565d5d8faf33601288
Alex Kasko <alex@staticlibs.net>
2025-03-31 01:10:23 +0100

    Implement ParameterMetadata
    This PR brings in the change that was contributed earlier by
    `eramitmittal` in #114 but was not merged. That change is rebased onto
    the latest `main`with minimal adjustetments.
    
    Testing: in addition to the `DECIMAL` test from original PR more tests
    are added to cover all other column types.
    
    Fixes: #121
    Fixes: #41
    
    Co-authored-by: eramitmittal <amit.mittal.github@gmail.com>

--- src/main/java/org/duckdb/DuckDBParameterMetaData.java
+++ src/main/java/org/duckdb/DuckDBParameterMetaData.java
@@ -2,13 +2,20 @@ package org.duckdb;
 
 import java.sql.ParameterMetaData;
 import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
 
 public class DuckDBParameterMetaData implements ParameterMetaData {
-    private DuckDBResultSetMetaData meta;
 
-    public DuckDBParameterMetaData(DuckDBResultSetMetaData meta) {
-        this.meta = meta;
+    private int param_count;
+    private DuckDBColumnType[] param_types;
+    private DuckDBColumnTypeMetaData[] param_types_meta;
+    private String[] param_types_string;
+
+    public DuckDBParameterMetaData(int param_count, String[] param_types_string, DuckDBColumnType[] param_types,
+                                   DuckDBColumnTypeMetaData[] param_types_meta) {
+        this.param_count = param_count;
+        this.param_types_string = param_types_string;
+        this.param_types = param_types;
+        this.param_types_meta = param_types_meta;
     }
 
     @Override
@@ -23,7 +30,7 @@ public class DuckDBParameterMetaData implements ParameterMetaData {
 
     @Override
     public int getParameterCount() throws SQLException {
-        return meta.param_count;
+        return param_count;
     }
 
     @Override
@@ -33,35 +40,60 @@ public class DuckDBParameterMetaData implements ParameterMetaData {
 
     @Override
     public boolean isSigned(int param) throws SQLException {
-        return true;
+        if (param > param_count) {
+            throw new SQLException("Parameter index out of bounds");
+        }
+        return DuckDBResultSetMetaData.is_signed(param_types[param - 1]);
     }
 
     @Override
     public int getPrecision(int param) throws SQLException {
-        throw new SQLFeatureNotSupportedException("getPrecision");
+        if (param > param_count) {
+            throw new SQLException("Parameter index out of bounds");
+        }
+        DuckDBColumnTypeMetaData typeMetaData = param_types_meta[param - 1];
+        if (typeMetaData == null) {
+            return 0;
+        }
+
+        return typeMetaData.width;
     }
 
     @Override
     public int getScale(int param) throws SQLException {
-        throw new SQLFeatureNotSupportedException("getScale");
+        if (param > param_count) {
+            throw new SQLException("Parameter index out of bounds");
+        }
+        DuckDBColumnTypeMetaData typeMetaData = param_types_meta[param - 1];
+        if (typeMetaData == null) {
+            return 0;
+        }
+
+        return typeMetaData.scale;
     }
 
     @Override
     public int getParameterType(int param) throws SQLException {
-        // TODO Auto-generated method stub
-        return 0;
+        if (param > param_count) {
+            throw new SQLException("Parameter index out of bounds");
+        }
+        return DuckDBResultSetMetaData.type_to_int(param_types[param - 1]);
     }
 
     @Override
     public String getParameterTypeName(int param) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        if (param > param_count) {
+            throw new SQLException("Parameter index out of bounds");
+        }
+        return param_types_string[param - 1];
     }
 
     @Override
     public String getParameterClassName(int param) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        if (param > param_count) {
+            throw new SQLException("Parameter index out of bounds");
+        }
+        return DuckDBResultSetMetaData.type_to_javaString(param_types[param - 1]);
     }
 
     @Override
---
+++ src/test/java/org/duckdb/TestParameterMetadata.java
@@ -0,0 +1,139 @@
+package org.duckdb;
+
+import static org.duckdb.TestDuckDBJDBC.JDBC_URL;
+import static org.duckdb.test.Assertions.*;
+import static org.duckdb.test.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.HashMap;
+
+public class TestParameterMetadata {
+
+    public static void test_parameter_metadata() throws Exception {
+        try (Connection conn = DriverManager.getConnection(JDBC_URL); Statement stmt = conn.createStatement()) {
+            for (DuckDBColumnType colType : DuckDBColumnType.values()) {
+                final String colTypeName;
+                switch (colType) {
+                case ENUM:
+                case MAP:
+                case STRUCT:
+                case UNION:
+                case UNKNOWN:
+                    continue;
+                case DECIMAL:
+                    colTypeName = "DECIMAL(18,3)";
+                    break;
+                case TIMESTAMP_WITH_TIME_ZONE:
+                    colTypeName = "TIMESTAMP WITH TIME ZONE";
+                    break;
+                case TIME_WITH_TIME_ZONE:
+                    colTypeName = "TIME WITH TIME ZONE";
+                    break;
+                case LIST:
+                case ARRAY:
+                    colTypeName = "VARCHAR[]";
+                    break;
+                default:
+                    colTypeName = colType.name();
+                }
+                String tblName = "metadata_test_" + colType.name();
+                stmt.execute("CREATE TABLE " + tblName + " (col1 " + colTypeName + ")");
+                try (PreparedStatement ps = conn.prepareStatement("INSERT INTO " + tblName + " VALUES(?)")) {
+                    ParameterMetaData meta = ps.getParameterMetaData();
+                    assertEquals(meta.getParameterTypeName(1), colTypeName);
+                }
+            }
+        }
+    }
+
+    public static void test_parameter_metadata_decimal() throws Exception {
+        try (Connection conn = DriverManager.getConnection(JDBC_URL)) {
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute("CREATE TABLE test_meta_decimal (id DECIMAL(3,0), dec16 DECIMAL(4,1), "
+                             + "dec32 DECIMAL(9,4), dec64 DECIMAL(18,7), "
+                             + "dec128 DECIMAL(38,10), int INTEGER, uint UINTEGER)");
+            }
+            try (
+                PreparedStatement ps = conn.prepareStatement(
+                    "INSERT INTO test_meta_decimal (id, dec16, dec32, dec64, dec128, int, uint) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
+                ParameterMetaData meta = ps.getParameterMetaData();
+                assertEquals(3, meta.getPrecision(1));
+                assertEquals(0, meta.getScale(1));
+                assertEquals(4, meta.getPrecision(2));
+                assertEquals(1, meta.getScale(2));
+                assertEquals(9, meta.getPrecision(3));
+                assertEquals(4, meta.getScale(3));
+                assertEquals(18, meta.getPrecision(4));
+                assertEquals(7, meta.getScale(4));
+                assertEquals(38, meta.getPrecision(5));
+                assertEquals(10, meta.getScale(5));
+                assertEquals(0, meta.getPrecision(6));
+                assertEquals(0, meta.getScale(6));
+                assertEquals(0, meta.getPrecision(7));
+                assertEquals(0, meta.getScale(7));
+
+                assertTrue(meta.isSigned(5));
+                assertTrue(meta.isSigned(6));
+                assertFalse(meta.isSigned(7));
+
+                assertTrue(BigDecimal.class.getName().equals(meta.getParameterClassName(1)));
+                assertTrue(Integer.class.getName().equals(meta.getParameterClassName(6)));
+                assertTrue(Long.class.getName().equals(meta.getParameterClassName(7)));
+            }
+        }
+    }
+
+    public static void test_parameter_metadata_struct() throws Exception {
+        try (Connection conn = DriverManager.getConnection(JDBC_URL); Statement stmt = conn.createStatement()) {
+            stmt.execute("CREATE TABLE metadata_test_struct_1 (col1 STRUCT(v VARCHAR, i INTEGER))");
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO metadata_test_struct_1 VALUES(?)")) {
+                ParameterMetaData meta = ps.getParameterMetaData();
+                assertEquals(meta.getParameterTypeName(1), "STRUCT(v VARCHAR, i INTEGER)");
+                assertEquals(meta.getParameterClassName(1), DuckDBStruct.class.getName());
+                assertEquals(meta.getPrecision(1), 0);
+                assertEquals(meta.getScale(1), 0);
+            }
+        }
+    }
+
+    public static void test_parameter_metadata_enum() throws Exception {
+        try (Connection conn = DriverManager.getConnection(JDBC_URL); Statement stmt = conn.createStatement()) {
+            stmt.execute("CREATE TYPE metadata_test_enum_type_1 as ENUM('foo', 'bar')");
+            stmt.execute("CREATE TABLE metadata_test_enum_1 (col1 metadata_test_enum_type_1)");
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO metadata_test_enum_1 VALUES(?)")) {
+                ParameterMetaData meta = ps.getParameterMetaData();
+                assertEquals(meta.getParameterTypeName(1), "ENUM");
+                assertEquals(meta.getParameterClassName(1), String.class.getName());
+                assertEquals(meta.getPrecision(1), 0);
+                assertEquals(meta.getScale(1), 0);
+            }
+        }
+    }
+
+    public static void test_parameter_metadata_map() throws Exception {
+        try (Connection conn = DriverManager.getConnection(JDBC_URL); Statement stmt = conn.createStatement()) {
+            stmt.execute("CREATE TABLE metadata_test_map_1 (col1 MAP(INTEGER, DOUBLE))");
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO metadata_test_map_1 VALUES(?)")) {
+                ParameterMetaData meta = ps.getParameterMetaData();
+                assertEquals(meta.getParameterTypeName(1), "MAP(INTEGER, DOUBLE)");
+                assertEquals(meta.getParameterClassName(1), HashMap.class.getName());
+                assertEquals(meta.getPrecision(1), 0);
+                assertEquals(meta.getScale(1), 0);
+            }
+        }
+    }
+
+    public static void test_parameter_metadata_union() throws Exception {
+        try (Connection conn = DriverManager.getConnection(JDBC_URL); Statement stmt = conn.createStatement()) {
+            stmt.execute("CREATE TABLE metadata_test_union_1 (col1 UNION(num INTEGER, str VARCHAR))");
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO metadata_test_union_1 VALUES(?)")) {
+                ParameterMetaData meta = ps.getParameterMetaData();
+                assertEquals(meta.getParameterTypeName(1), "UNION(num INTEGER, str VARCHAR)");
+                assertEquals(meta.getParameterClassName(1), String.class.getName());
+                assertEquals(meta.getPrecision(1), 0);
+                assertEquals(meta.getScale(1), 0);
+            }
+        }
+    }
+}
