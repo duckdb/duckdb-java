eb8f79ba6d5661d2342b0ca66bfe0c1cb1aa67f6
Alex Kasko <alex@staticlibs.net>
2026-05-16 11:36:46 +0100

    Re-enable jemalloc on Linux (main)
    This is a forward-port of the PR #686 to the `main` branch.
    
    This PR enables back `jemalloc` allocator for Linux builds applying the
    necessary changes to its packaging after duckdb/duckdb#22558.

--- vendor.py
+++ vendor.py
@@ -5,6 +5,9 @@ import pickle
 import platform
 import argparse
 
+def sanitize_path(x):
+    return x.replace('\\', '/')
+
 parser = argparse.ArgumentParser(description='Inlines DuckDB Sources')
 
 parser.add_argument('--duckdb', action='store',
@@ -12,8 +15,7 @@ parser.add_argument('--duckdb', action='store',
 
 args = parser.parse_args()
 
-# list of extensions to bundle, we include jemalloc here to have its
-# files copied along with all other sources
+# list of extensions to bundle
 extensions = ['core_functions', 'parquet', 'icu', 'json']
 
 # path to target
@@ -31,16 +33,28 @@ defines = ['DUCKDB_EXTENSION_{}_LINKED'.format(ext.upper()) for ext in extension
 
 # fresh build - copy over all of the files
 (source_list, include_list, original_sources) = package_build.build_package(target_dir, extensions, False)
-
-source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x)
-    for x in source_list]
-include_list = [os.path.join('src', 'duckdb', x) for x in include_list]
-
-def sanitize_path(x):
-    return x.replace('\\', '/')
-
 source_list = [sanitize_path(x) for x in source_list]
 include_list = [sanitize_path(x) for x in include_list]
+
+# process jemalloc separately with its own CMake vars
+jemalloc_source_list = [x for x in source_list if x.startswith("duckdb/third_party/jemalloc/")]
+jemalloc_include_list = [x for x in include_list if x.startswith("third_party/jemalloc/")]
+
+# clean up paths
+source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x)
+    for x in source_list if x not in jemalloc_source_list]
+source_list = [x.replace("/./", "/") for x in source_list]
+jemalloc_source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x)
+    for x in jemalloc_source_list]
+jemalloc_source_list = [x for x in jemalloc_source_list if not x.endswith("jemalloc_cpp.cpp")]
+include_list = [os.path.join('src', 'duckdb', x) for x in include_list if x not in jemalloc_include_list]
+jemalloc_include_list = [os.path.join('src', 'duckdb', x) for x in jemalloc_include_list]
+
+# sort paths
+source_list.sort()
+jemalloc_source_list.sort()
+include_list.sort()
+jemalloc_include_list.sort()
 
 os.chdir(basedir)
 
@@ -51,7 +65,9 @@ def replace_entries(cmake, replacement_map):
     cmake.replace()
 
 cmake = cmake.replace('${SOURCES}', '\n  '.join(source_list))
+cmake = cmake.replace('${JEMALLOC_SOURCES}', '\n  '.join(jemalloc_source_list))
 cmake = cmake.replace('${INCLUDES}', '\n  '.join(include_list))
+cmake = cmake.replace('${JEMALLOC_INCLUDES}', '\n  '.join(jemalloc_include_list))
 cmake = cmake.replace('${DEFINES}', '\n  '.join(['-D'+x for x in defines]))
 
 with open('CMakeLists.txt', 'w+') as f:
